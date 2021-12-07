66263f21a4c29264a4c62de606890385a19c61ce
nuancce <ksenija.mackarova2014@yandex.ru>
2021-12-07 23:30:10 +0300

    доделала

---
+++ homework/LotsofBooks.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    public delegate void DelegatSorted(int temp);
+    class LotsofBooks
+    {
+        public Book[] books = new Book[] { new Book {Name="Idiot", Author="Dotoyevsky", Publisher="Kniga" },
+            new Book {Name="Voina i mir", Author="Tolstoi", Publisher="Klassika"},
+            new Book { Name = "Nyanya", Author = "Pushkin", Publisher = "Detskaya Literatura" }};
+        public void SortingBooks(DelegatSorted deleg, int i)
+        {
+            deleg.Invoke(i);
+        }
+        public void Sort(int temp)
+        {
+            if (temp == 1)
+            {
+                Console.Write("По названию ");
+                books = books.OrderBy(e => e.Name).ToArray();
+            }
+            else if (temp == 2)
+            {
+                Console.Write("По автору ");
+                books = books.OrderBy(e => e.Author).ToArray();
+            }
+            else
+            {
+                Console.Write("По издательству ");
+                books = books.OrderBy(e => e.Publisher).ToArray();
+            }
+            Console.WriteLine("отсортированныи массив");
+            foreach (var item in books)
+            {
+                Console.WriteLine("Название - {0}, автор - {1}, издательство - {2}", item.Name, item.Author, item.Publisher);
+            }
+        }
+    }
+}
--- homework/Program.cs
+++ homework/Program.cs
@@ -11,6 +11,12 @@ namespace homework
         static void Main(string[] args)
         {
             Console.WriteLine("Домашнее задание 12.1");
+
+            Console.WriteLine("Домашнее задание 12.2");
+            LotsofBooks lostofBooks = new LotsofBooks();
+            lostofBooks.SortingBooks(new DelegatSorted(lostofBooks.Sort), 1);
+            lostofBooks.SortingBooks(new DelegatSorted(lostofBooks.Sort), 2);
+            lostofBooks.SortingBooks(new DelegatSorted(lostofBooks.Sort), 3);
             Console.ReadKey();
         }
     }
